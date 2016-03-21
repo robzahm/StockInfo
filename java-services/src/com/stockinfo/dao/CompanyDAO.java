78fac9ba87c5cb47451ace7d07177bf22c273100
Robert Zahm <robzahm@gmail.com>
2016-03-20 21:18:04 -0500

    Prevent symbols with carats from being shown

--- java-services/src/com/stockinfo/dao/CompanyDAO.java
+++ java-services/src/com/stockinfo/dao/CompanyDAO.java
@@ -12,7 +12,8 @@ import com.stockinfo.api.Company;
 @RegisterMapper(CompanyMapper.class)
 public interface CompanyDAO {
 	// TODO: Add exchange
-	@SqlQuery("select symbol, name from company where name like concat(:name, '%') order by name asc limit 50")
+	// Filter out symbols with carats - this looks like bad data in the CSVs, and we can't get quotes for it
+	@SqlQuery("select symbol, name from company where name like concat(:name, '%') and symbol not like '%^%' order by name asc limit 50")
 	List<Company> findByName(@Bind("name") String name);
 }
 
