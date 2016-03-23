d4f7bc14d1913f35004e49d8ba08997334e287af
Robert Zahm <robzahm@gmail.com>
2016-03-22 21:54:01 -0500

    Database create/import script

--- data/importData.sh
+++ data/importData.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-/usr/local/mysql/bin/mysqlimport --fields-terminated-by=, --fields-enclosed-by='"' --ignore-lines=1 --user root --password stockInfo ${PWD}/company.csv1 ${PWD}/company.csv2 ${PWD}/company.csv3
+# Validate that 2 parameters were passed in, and display them to the user
+if [ $# -eq 2 ]
+  then
+    echo "Executing script as user: $1 with password: $2"
+  else
+  	echo "Usage: ./importData.sh username password"
+  	echo "User must have 'create database' privileges"
+  	exit
+fi
+
+# Create the database schema
+/usr/local/mysql/bin/mysql --user $1 --password=$2 < ${PWD}/Schema.txt
+
+# Create the company table
+/usr/local/mysql/bin/mysql --user $1 --password=$2 stockinfo < ${PWD}/DDL.txt
+
+# Load data into the company table
+/usr/local/mysql/bin/mysqlimport --fields-terminated-by=, --fields-enclosed-by='"' --ignore-lines=1 --user $1 --password=$2 stockInfo ${PWD}/company.csv1 ${PWD}/company.csv2 ${PWD}/company.csv3
